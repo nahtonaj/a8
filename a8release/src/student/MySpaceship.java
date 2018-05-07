21a27b9a8a92edaf146c4492394b84bd703a9798
Kokopuffsss <38254431+Kokopuffsss@users.noreply.github.com>
2018-05-07 17:16:18 -0400

    Add files via upload

--- a8release/src/student/MySpaceship.java
+++ a8release/src/student/MySpaceship.java
@@ -16,9 +16,10 @@ import controllers.RescuePhase;
 
 /** An instance implements the methods needed to complete the mission. */
 public class MySpaceship implements Spaceship {
-	private HashMap<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
-	private LinkedList<Integer> path = new LinkedList<Integer>();
-
+	private HashMap<Integer, Boolean> visited;
+	private LinkedList<Integer> path;
+	private HashMap<Node, Boolean> depleted;
+	
 	/** The spaceship is on the location given by parameter state.
 	 * Move the spaceship to Planet X and then return (with the spaceship is on
 	 * Planet X). This completes the first phase of the mission.
@@ -46,32 +47,41 @@ public class MySpaceship implements Spaceship {
 	@Override
 	public void search(SearchPhase state) {
 		// TODO: Find the missing spaceship
+		visited = new HashMap<Integer, Boolean>();
+		path = new LinkedList<Integer>();
+		find(state);
+	}
+	
+	private void find(SearchPhase state) {
 		if(state.onPlanetX())
 			return;
 		if(!visited.containsKey(state.currentID()))
 			visited.put(state.currentID(), true);
-		NodeStatus ns = max(state.neighbors(), state);
+		NodeStatus next = bestNeighbor(state);
+		path.addFirst(state.currentID());
+		state.moveTo(next.id());
+		find(state);
+	}
+	
+	private NodeStatus bestNeighbor(SearchPhase state) {
+		NodeStatus ns = maxSignalNode(state.neighbors());
 		while(ns == null) {
 			visited.put(state.currentID(), true);
 			state.moveTo(path.getFirst());
 			path.removeFirst();
-			ns = max(state.neighbors(), state);
+			ns = maxSignalNode(state.neighbors());
 		}
-		path.addFirst(state.currentID());
-		state.moveTo(ns.id());
-		search(state);
-		
+		return ns;
 	}
 	
-	private NodeStatus max(NodeStatus[] n, SearchPhase state) {
+	private NodeStatus maxSignalNode(NodeStatus[] n) {
 		NodeStatus best = null;
 		for(NodeStatus ns: n) {
-			if(!visited.containsKey(ns.id())) {
-				if(best == null) best = ns;
-				else if(ns.compareTo(best) > 0) {
-					best = ns;
-				}
-			}
+			if(visited.containsKey(ns.id()))
+				continue;
+			if(best == null) best = ns;
+			if(ns.compareTo(best) > 0)
+				best = ns;
 		}
 		return best;
 	}
@@ -95,73 +105,65 @@ public class MySpaceship implements Spaceship {
 	@Override
 	public void rescue(RescuePhase state) {
 		// TODO: Complete the rescue mission and collect gems
-		LinkedList<Node> shortestPathtoEarth = (LinkedList<Node>) Paths.minPath(state.currentNode(), state.earth());
-		LinkedList<Node> gemPath = addGemPath(shortestPathtoEarth, state);
-		gemPath.poll();
-		while(gemPath != null && !gemPath.isEmpty()) {
-			state.moveTo(gemPath.poll());
-		}
-		
+		depleted = new HashMap<Node, Boolean>();
+		backToEarth(state);
 	}
 	
-	public LinkedList<Node> addGemPath(LinkedList<Node> ll, RescuePhase state) {
-		if(ll.isEmpty()) {
-			return null;
+	private void backToEarth(RescuePhase state) {
+		if(state.currentNode() == state.earth()) {
+			return;
 		}
-		int sumLengths = 0;
-		int maxLength = 0;
-		int maxLengthIndex = 0;
-		int currLength = 0;
-		int maxGemIndex = 0;
-		int sumGems = 0;
-		int gemfuelratio = 1;
-		int currGems = ll.get(0).gems();
-		int maxGems = currGems;
-		LinkedList<Integer> lengths = new LinkedList<Integer>();
-		
-		Node currNode = null;
-//		for(int i=1; i<ll.size(); i++) {
-//			currNode = ll.get(i);
-//			currGems = currNode.gems();
-//			currLength = ll.get(i-1).getEdge(currNode).fuelNeeded();
-//			lengths.add(currLength);
-//			sumLengths += currLength;
-//			sumGems += currGems;
-//			if(currGems > maxGems) maxGems = currGems; maxGemIndex = i;
-//			if(currLength > maxLength) maxLength = currLength; maxLengthIndex = i-1;
-//		}
-		sumLengths = Paths.pathWeight(ll);
-
-		Edge nextEdge = null;
-		Edge prevEdge = null;
-		int extensionlength = 0;
-		int currentlength = 0;
-		for(int j = ll.size()-2; j > 1; j--) {
-			currNode = ll.get(j);
-			for(Node n : currNode.neighbors().keySet()) {
-				nextEdge = n.getEdge(ll.get(j+1));
-				prevEdge = n.getEdge(ll.get(j-1));
-				if(nextEdge!=null && prevEdge!=null) {
-					extensionlength = prevEdge.length + nextEdge.length;
-					if(j+1 < lengths.size()) {
-						currentlength = lengths.get(j+1)+lengths.get(j);
-					} else {
-						currentlength = lengths.get(j);
-					}
-					if(//n.gems()/extensionlength > currGems/currentlength
-							 sumLengths+extensionlength < state.fuelRemaining()) {
-						//ll.remove(j);
-						ll.add(j, n);
-						List<Node> pathBack = Paths.minPath(n, ll.get(j+1));
-						for(int k=0; k<pathBack.size(); k++) {
-							ll.add(j+k+1, pathBack.get(k));
-						}
-						System.out.println("added node");
-					}
-				}
+		Node n = bestNode(state);
+		depleted.put(state.currentNode(), true);
+		state.moveTo(n);
+		backToEarth(state);
+	}
+	
+	private Node bestNode(RescuePhase state) {
+		HashMap<Node, Integer> neighbors = state.currentNode().neighbors();
+		double max = 0;
+		double g = 0;
+		double minlength = 0;
+		double edge = 0;
+		double num = 0;
+		double index = 0;
+		Node best = null;
+		for(Node n: neighbors.keySet()) {
+			if(depleted.containsKey(n)) {
+				g = 0;
 			}
-		} return ll;
-		
+			else {
+				g = n.gems();
+			}
+			LinkedList<Node> ll = (LinkedList) Paths.minPath(n, state.earth());
+			minlength = pathLength(ll);
+			num = ll.size();
+			edge = neighbors.get(n);
+			if(edge + minlength > state.fuelRemaining()) {
+				continue;
+			}
+			index = (Math.pow(g, 1)*Math.pow(minlength, 50)*Math.pow(num, 1))/edge;
+			if(index > max) {
+				max = index;
+				best = n;
+			}
+		}
+		if(best == null) {
+			LinkedList<Node> backup = (LinkedList) Paths.minPath(state.currentNode(), state.earth());
+			backup.removeLast();
+			return backup.getLast();
+		}
+		return best;
 	}
 	
+	private double pathLength(LinkedList<Node> path) {
+		Iterator<Node> it1 = path.descendingIterator();
+		Iterator<Node> it2 = path.descendingIterator();
+		it2.next();
+		double sum = 0;
+		while(it2.hasNext()) {
+			sum += it1.next().getEdge(it2.next()).length;
+		}
+		return sum;
+	}
 }
